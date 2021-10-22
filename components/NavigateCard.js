7b8fc93cc49ccdc494e643e7cbe55f4069b9d6c4
Luciano Aguilera <49349263+Northzedam@users.noreply.github.com>
2021-10-22 00:24:13 -0300

    ride options funcionando

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import tw from 'tailwind-react-native-classnames';
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
 import { setDestination } from '../slices/navSlice';
 import { useNavigation } from '@react-navigation/core';
+import NavFavourites from './NavFavourites';
+import { Icon } from 'react-native-elements';
+
 
 const NavigateCard = () => {
 
@@ -16,28 +19,48 @@ const NavigateCard = () => {
         <SafeAreaView style={tw`bg-white flex-1`}>
             <Text style={tw`text-center py-5 text-xl`}>Good morning, Sonny</Text>
             <View style={tw`border-t border-gray-200 flex-shrink`}>
-                <GooglePlacesAutocomplete
-                placeholder="Where to?"
-                styles={toInputBoxStyles}
-                fetchDetails={true}
-                returnKeyType={"search"}
-                minLength={2}
-                onPress={(data, details=null) => {
-                    dispatch(setDestination({
-                        location: details.geometry.location,
-                        description: data.description
-                    }))
-                    navigation.navigate("RideOptionsCard")
-                }}
-                enablePoweredByContainer={false}
-                query={{
-                    key: GOOGLE_MAPS_APIKEY,
-                    language:"en", 
-                }}
-                nearbyPlacesAPI="GooglePlacesSearch"
-                debounce={400}
-                />
-                    
+                <View>
+                    <GooglePlacesAutocomplete
+                    placeholder="Where to?"
+                    styles={toInputBoxStyles}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    minLength={2}
+                    onPress={(data, details=null) => {
+                        dispatch(setDestination({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+                        navigation.navigate("RideOptionsCard")
+                    }}
+                    enablePoweredByContainer={false}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language:"en", 
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                    />
+                </View>
+                <NavFavourites />
+            </View>
+
+            <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-top border-gray-100`}>
+                <TouchableOpacity
+                onPress={() => navigation.navigate("RideOptionsCard")}
+                style={tw`flex flex-row bg-black w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon name="car" type="font-awesome" color="white" size={16} />
+                    <Text style={tw`text-white text-center`}>Rides</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
+                >
+                    <Icon name="fast-food-outline" type="ionicon" color="black" size={16} />
+                    <Text style={tw`text-center`}>Eats</Text>
+                </TouchableOpacity>
+                  
             </View>
         </SafeAreaView>
     );
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,67 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, Text, SafeAreaView, TouchableOpacity, FlatList, View, Image } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+import { Icon} from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native'; 
+const data = [
+    {
+        id: "Uber-X-123",
+        title: "UberX",
+        multiplier: 1,
+        image: "https://links.papareact.com/3pn"
+    },
+    {
+        id: "Uber-X-456",
+        title: "UberXL",
+        multiplier: 1.2,
+        image: "https://links.papareact.com/5w8"
+    },
+    {
+        id: "Uber-X-789",
+        title: "UberLUX",
+        multiplier: 1.75,
+        image: "https://links.papareact.com/7pf"
+    },
+];
 
 const RideOptionsCard = () => {
+    const navigation = useNavigation();
+    const [selected, setSelected] = useState(null);
+
     return (
-        <View>
-            <Text>pick a ride</Text>
-        </View>
+        <SafeAreaView style={tw`bg-white flex-grow`}>
+            <View>
+            <TouchableOpacity
+             style={tw`absolute top-3 left-5 p-3 rounded-full`}
+             onPress={() => navigation.navigate("NavigateCard")}
+             >
+                <Icon name="chevron-left" type="fontawesome" />       
+            </TouchableOpacity>
+            <Text stlye={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+            </View>
+            <FlatList data = {data} keyExtractor = { (item) => item.id }
+                 renderItem={({item: {id,title,multiplier,image}, item }) => (
+                    <TouchableOpacity style={tw`flex-row justify-between items-center px-10` }
+                    onPress={() => (setSelected(item))}
+                    >
+                     <Image 
+                     style={{
+                         width: 100,
+                         height: 100,
+                         resizeMode: "contain",
+                     }}
+                     source={{uri: image}}
+                     />
+                     <View style={tw`ml-6`}>
+                         <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                         <Text>Travel Time...</Text>
+                     </View>
+                     <Text style={tw`text-xl`}>$100</Text>
+                    </TouchableOpacity>
+                 )}
+
+            />
+        </SafeAreaView>
     )
 }
 
